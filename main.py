ad632e045fda5a23a5fa4d3657fc0fad936de054
Seth Scott <s.scottvancouverwa@gmail.com>
2022-05-15 13:38:47 -0700

    separate api and scraper into different classes/files

--- main.py
+++ main.py
@@ -1,61 +1,10 @@
-import os
-import datetime
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from flask import Flask, jsonify
-
-# TODO separate flask and API into different classes in different files
 # TODO run selenium headless
 # TODO build docker with dependencies (unraid)
 # TODO run api program on cronjob
+# TODO get rid of main.py
 
 
-app = Flask(__name__)
 
 
-INSTANCE = os.getenv("INSTANCE")
-DISTRICT = os.getenv("DISTRICT")
-SCHOOL = os.getenv("SCHOOL")
-MENU = os.getenv("MENU")
 
 
-CHROME_DRIVER_PATH = "/Users/sethscott/Documents/python/chromedriver"
-SERVICE = Service(CHROME_DRIVER_PATH)
-DRIVER = webdriver.Chrome(service=SERVICE)
-
-CURRENT_DAY = datetime.datetime.now().strftime("%d")
-ISO_DATE = datetime.datetime.now().strftime("%Y-%m-%d")
-
-
-def scrape():
-    DRIVER.get(f"https://www.myschoolmenus.com/instance/{INSTANCE}/district/{DISTRICT}/school/{SCHOOL}/menu/{MENU}")
-    DRIVER.implicitly_wait(10)
-
-    dates = DRIVER.find_elements(By.CLASS_NAME, "calendar-day")
-    food = DRIVER.find_elements(By.CLASS_NAME, "menu-entrees")
-
-    lunches = [lunch.text for lunch in food]
-    days = [date.text for date in dates]
-    lunch_menu = {days[i]: lunches[i] for i in range(len(lunches))}
-
-    return lunch_menu
-
-
-menu = scrape()
-
-
-try:
-    today = menu[str(CURRENT_DAY)]
-except KeyError:
-    print("No lunch today")
-DRIVER.quit()
-
-
-@app.route('/')
-def api():
-    return jsonify(menu)
-
-
-if __name__ == '__main__':
-    app.run(port=5001, host="0.0.0.0")
---
+++ scraper.py
@@ -0,0 +1,45 @@
+# MAKE SURE ENVIRONMENT VARIABLES ARE LOADED INTO VENV
+import os
+import datetime
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import json
+
+INSTANCE = os.getenv("INSTANCE")
+DISTRICT = os.getenv("DISTRICT")
+SCHOOL = os.getenv("SCHOOL")
+MENU = os.getenv("MENU")
+
+CHROME_DRIVER_PATH = "/Users/sethscott/Documents/python/chromedriver"
+SERVICE = Service(CHROME_DRIVER_PATH)
+
+CURRENT_DAY = datetime.datetime.now().strftime("%d")
+ISO_DATE = datetime.datetime.now().strftime("%Y-%m-%d")
+
+
+class Scraper:
+
+    def __init__(self):
+        self.DRIVER = webdriver.Chrome(service=SERVICE)
+        self.DRIVER.get(
+            f"https://www.myschoolmenus.com/instance/{INSTANCE}/district/{DISTRICT}/school/{SCHOOL}/menu/{MENU}")
+        self.DRIVER.implicitly_wait(10)
+        self.dates = self.DRIVER.find_elements(By.CLASS_NAME, "calendar-day")
+        self.food = self.DRIVER.find_elements(By.CLASS_NAME, "menu-entrees")
+        self.lunches = [lunch.text for lunch in self.food]
+        self.days = [date.text for date in self.dates]
+        self.lunch_menu = {self.days[i]: self.lunches[i] for i in range(len(self.lunches))}
+
+    def scrape(self):
+        self.DRIVER.quit()
+        return self.lunch_menu
+
+
+scraper = Scraper()
+menu = scraper.scrape()
+
+with open('data.json', mode='w') as menu_api:
+    json.dump(menu, menu_api)
+
+print(menu)
