2a4457f8f1f99225c991c98efb943a1f5b705a07
Seth Scott <s.scottvancouverwa@gmail.com>
2022-11-06 22:43:21 -0800

    refactor scraper for compatibility

--- scraper/main.py
+++ scraper/main.py
@@ -10,7 +10,7 @@ get_date = GetDates()
 scraper = Scraper()
 
 MONGO_IP = os.getenv("MONGO_IP")
-MONGO_PORT = os.getenv("MONGO_PORT")
+MONGO_PORT = int(os.getenv("MONGO_PORT"))
 
 menu = scraper.scrape(get_date.tomorrow_iso_date.strftime("%Y-%m"))
 
