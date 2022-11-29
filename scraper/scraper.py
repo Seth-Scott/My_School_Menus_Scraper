a75754e339a05bce52dd91a40052dcd0ad4ff51e
Seth Scott <s.scottvancouverwa@gmail.com>
2022-11-28 22:12:15 -0800

    refactor string output to remove extraneous characters, implement character filtering function for further filtering

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from get_dates import GetDates
 import time
+import re
 
 get_date = GetDates()
 WEBDRIVER_ADDRESS = os.getenv("WEBDRIVER_ADDRESS")
@@ -39,7 +40,32 @@ class Scraper:
         self.driver.implicitly_wait(10)
         dates = self.driver.find_elements(By.CLASS_NAME, "calendar-day")
         food = self.driver.find_elements(By.CLASS_NAME, "menu-entrees")
-        lunches = [lunch.text.replace("Goldkist ", "").replace("Entree\n", "").replace("Vegetables\n", "").replace("Fruit and Vegetable Variety\n", "").replace("Grains\n", "").replace("Milk Choice\n", "").replace("Misc.\n", "").replace("Milk\n", "").replace("\nCondiments", "").replace("\n", " & ") for lunch in food]
+
+        def strip_extra_text(string):
+            forbidden_words = [
+                "Goldkist",
+                "Entree",
+                "Vegetables",
+                "Fruit and Vegetable Variety",
+                "Grains",
+                "Milk Choice",
+                "Misc.",
+                "Milk",
+                "Condiments",
+                "Corn Cobbette"
+                "Choice of"
+                "VeteransDay"
+                "ThanksgivingBreak"
+                "\n"
+            ]
+
+            for word in forbidden_words:
+                string = string.replace(word, " ")
+            string = re.sub(" +", " ", string).strip()
+            return string
+
+
+        lunches = [strip_extra_text(lunch.text) for lunch in food]
         # create a list of days and remove the empties
         days = [date.text for date in dates if date.text != ""]
         #  below creates a faux-ISO date with "i" (in the dictionary comprehension) rather than the hardcoded date, remove empties
